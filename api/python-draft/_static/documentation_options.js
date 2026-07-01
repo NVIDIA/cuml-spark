e72328ef9e5f7f6f4289a24a39e8b31b77f07553
spark-rapids automation <70000568+nvauto@users.noreply.github.com>
2026-07-01 03:41:08 +0000

    Update draft api docs to commit b6cb77a on

--- api/python-draft/_static/documentation_options.js
+++ api/python-draft/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '25.12.0',
+    VERSION: '26.06.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
